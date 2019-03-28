b662ff3f891f214715075cfecd57f078aac4f7b2
dengkaipeng <dengkaipeng@baidu.com>
2019-03-28 09:28:05 +0000

    add TSM model.

--- fluid/PaddleCV/video/models/__init__.py
+++ fluid/PaddleCV/video/models/__init__.py
@@ -2,12 +2,14 @@ from .model import regist_model, get_model
 from .attention_cluster import AttentionCluster
 from .nextvlad import NEXTVLAD
 from .tsn import TSN
+from .tsm import TSM
 from .stnet import STNET
 from .attention_lstm import AttentionLSTM
 
 # regist models
 regist_model("AttentionCluster", AttentionCluster)
-regist_model("NEXTVLAD", NEXTVLAD)
-regist_model("TSN", TSN)
-regist_model("STNET", STNET)
 regist_model("AttentionLSTM", AttentionLSTM)
+regist_model("NEXTVLAD", NEXTVLAD)
+regist_model("STNET", STNET)
+regist_model("TSN", TSN)
+regist_model("TSM", TSM)
