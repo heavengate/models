2c60bf8d69f4d5df1f0b19acbbbc3489807edae0
dengkaipeng <dengkaipeng@baidu.com>
2019-06-02 07:13:27 +0000

    fix mobilenet

--- PaddleCV/yolov3/models/mobilenet.py
+++ PaddleCV/yolov3/models/mobilenet.py
@@ -1,6 +1,7 @@
 import paddle.fluid as fluid
 from paddle.fluid.initializer import MSRA
 from paddle.fluid.param_attr import ParamAttr
+from paddle.fluid.regularizer import L2Decay
 
 
 def conv_bn(input,
@@ -32,8 +33,10 @@ def conv_bn(input,
     bn = fluid.layers.batch_norm(input=conv, 
                                  act=act,
                                  is_test=is_test,
-            			 param_attr=ParamAttr(name=bn_name + "_scale"),
-            			 bias_attr=ParamAttr(name=bn_name + "_offset"),
+            			 param_attr=ParamAttr(name=bn_name + "_scale",
+                                                      regularizer=L2Decay(0.)),
+            			 bias_attr=ParamAttr(name=bn_name + "_offset",
+                                                     regularizer=L2Decay(0.)),
             			 moving_mean_name=bn_name + '_mean',
             			 moving_variance_name=bn_name + '_variance')
     
@@ -76,15 +79,16 @@ def mobile_net(img, scale=1.0, is_test=True):
     tmp = depthwise_separable(tmp, 128, 128, 128, 1, scale, is_test=is_test, name='conv3_1')
     tmp = depthwise_separable(tmp, 128, 256, 128, 2, scale, is_test=is_test, name='conv3_2')
     # 1/8
-    blocks.append(tmp)
     # 38x38
     tmp = depthwise_separable(tmp, 256, 256, 256, 1, scale, is_test=is_test, name='conv4_1')
-    tmp = depthwise_separable(tmp, 256, 512, 256, 2, scale, is_test=is_test, name='conv4_2')
-    # 1/16
     blocks.append(tmp)
+    tmp = depthwise_separable(tmp, 256, 512, 256, 2, scale, is_test=is_test, name='conv4_2')
 
+    # 1/16
     for i in range(5):
         tmp = depthwise_separable(tmp, 512, 512, 512, 1, scale, is_test=is_test, name='conv5_'+str(i+1))
+    blocks.append(tmp)
+
     # 19x19
     tmp = depthwise_separable(tmp, 512, 1024, 512, 2, scale, is_test=is_test, name='conv5_6')
     tmp = depthwise_separable(tmp, 1024, 1024, 1024, 1, scale, is_test=is_test, name='conv6')
